e536e6801412357a8996e185e2203a12423d1eff
Alberto Poljak <albertopoljak@gmail.com>
2017-06-20 14:51:38 +0200

    Updated code to support ASCII art

--- src/xopher/hangman/Game.java
+++ src/xopher/hangman/Game.java
@@ -12,7 +12,8 @@ import java.util.List;
 public class Game {
 
     public static void main(String[] args) throws FileNotFoundException {
-
+        Scanner game_state = new Scanner(new FileReader("resources/game_state.txt"));
+        
         Scanner in = new Scanner(new FileReader("resources/words.txt"));
         List<String> words = new ArrayList<String>();
         while (in.hasNextLine()) {
@@ -24,6 +25,21 @@ public class Game {
         // System.out.println(Arrays.toString(wordsArray));
 
         System.out.println("-=+ W E L C O M E   T O   H A N G M A N +=-");
-
+        printGameStateString(game_state);
+        /*
+         * Game code here
+         * */
+        
+        game_state.close();
     }
+    
+    
+    //Each time it's called it will print next set of characters surrounded by ; and ; in file game_state.txt
+    private static void printGameStateString(Scanner state) throws FileNotFoundException{
+    	if( state.hasNext() ){
+	    	state.useDelimiter("\\s*;\\s*");
+	    	System.out.println(state.next());
+    	}
+    }
+    
 }
