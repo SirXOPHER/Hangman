8b13881a3f3de8c404a137b76d9e1e5161a7ce3e
XOPHER <chris.reinmuller@gmail.com>
2017-06-21 14:23:48 +0100

    Basic implementation of the game

--- src/main/java/hangman/Game.java
+++ src/main/java/hangman/Game.java
@@ -5,39 +5,126 @@ import java.io.FileReader;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-/* Hangman is a Java implementation of the classic paper and pencil guessing game.  */
+/* Hangman is a Java implementation of the classic paper and pencil guessing game. */
 
 public class Game {
 
+    private static int stage = 0;       // stage of current round
+    private static List<String> words;  // pool of words
+    private static String word;         // plaintext word for current round
+    private static String hidden;       // masked word
+
     public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(new FileReader("src/main/resources/words.txt"));
+        words = new ArrayList<String>();
+        while (input.hasNextLine()) {
+            words.add(input.nextLine());
+        }
+        input.close();
+
+        System.out.println("-=+   W E L C O M E   T O   H A N G M A N   +=-");
+
+        playRound();
+
+        // Issue: playAgain() method will not take input (Scanner issue?)
+        // playAgain() method takes input if first round is omitted!?!
+        // Block handling the replay loop deactivated
+        /*
+        Scanner repeat = new Scanner(System.in);
+        boolean finished = false;
+        do {
+            if (playAgainPrompt(repeat) == true) {
+                System.out.println(" \n \n" + "Alright, here we go again!");
+                stage = 0;
+                playRound();
+            } else {
+                System.out.println(" \n \n" + "Okay, let's call it a day! Thanks for playing Hangman!");
+                finished = true;
+            }
+        } while (!finished);
+        repeat.close();
+        */
+    }
+
+    private static void playRound() throws FileNotFoundException {
         Scanner game_state = new Scanner(new FileReader("src/main/resources/game_state.txt"));
         game_state.useDelimiter("\\s*;\\s*");
-        Scanner in = new Scanner(new FileReader("src/main/resources/words.txt"));
-        List<String> words = new ArrayList<String>();
-        while (in.hasNextLine()) {
-            words.add(in.nextLine());
+        printGameStateString(game_state); // show title screen on console (first in game_state.txt)
+        Scanner user = new Scanner(System.in);
+        Random pick = new Random();
+        int  n = pick.nextInt(100) + 1;
+        word = words.get(n); // to-do: replace with .remove() and handle empty words list
+        hidden = new String(new char[word.length()]).replace("\0", "_");
+        int stage_tracker;
+
+        while (hidden.contains("_") && stage < 7) {
+            //System.out.println(" \n \n" + "ONLY FOR DEVELOPMENT // " + "STAGE:" + stage + " | " + "WORD:" + word);
+            System.out.println(" \n \n" + "HIDDEN WORD: " + hidden.replace("", " ").trim());
+            System.out.println(" \n \n" + "Take a guess (enter a letter A - Z)");
+
+            char c = user.next().toLowerCase().charAt(0);
+
+            stage_tracker = stage;
+            evaluateGuess(checkInput(c));
+            if (stage_tracker != stage) { printGameStateString(game_state); }
+
+            if (stage == 7) {
+                System.out.println(" \n \n" + "Oh no! You couldn't guess the word \"" + word + "\""
+                                            + " in time to escape the executioner!");
+            }
+
+            if (hidden.equals(word)) {
+                System.out.println(" \n \n" + "SOLUTION: " + word.replace("", " ").trim());
+                System.out.println(" \n \n" + "Congrats! You correctly guessed the word \"" + word + "\"");
+                break;
+            }
         }
-        in.close();
-
-        // String[] wordsArray = words.toArray(new String[words.size()]);
-        // System.out.println(Arrays.toString(wordsArray));
-
-        System.out.println("-=+ W E L C O M E   T O   H A N G M A N +=-");
-        printGameStateString(game_state);
-        /*
-         * Game code here
-         * */
-        
         game_state.close();
+        user.close();
+    }
+
+    private static String checkInput(char letter) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if      (hidden.charAt(i) != '_')   { sb.append(word.charAt(i)); }
+            else if (letter == word.charAt(i))  { sb.append(letter); }
+            else                                { sb.append("_"); }
+        }
+        return sb.toString();
+    }
+
+    private static void evaluateGuess(String checked) {
+        if      (hidden.equals(checked)) { stage++; }
+        else                             { hidden = checked; }
     }
     
-    
-    //Each time it's called it will print next set of characters surrounded by ; and ; in file game_state.txt
+    // Each time it's called it will print next set of characters surrounded by ; and ; in file game_state.txt
     private static void printGameStateString(Scanner state) throws FileNotFoundException{
     	if( state.hasNext() ){
 	    	System.out.println(state.next());
     	}else
             System.out.println("Game has already ended!");
      }
+
+    private static boolean playAgainPrompt(Scanner again) {
+
+        boolean play = true;
+        boolean valid = false;
+
+        while(!valid)
+        {
+            System.out.println(" \n \n" + "Do you fancy another round of Hangman?");
+            System.out.print(" \n \n" + "YES [Y] or NO [N]:");
+            String input = again.nextLine();
+
+            if      (input.toLowerCase().equals("y")) { valid = true; }
+            else if (input.toLowerCase().equals("n")) { valid = true; play = false; }
+            else {
+                    System.out.println(" \n \n" + "Fat-finger error? Let's try this again:");
+            }
+        }
+        return play;
+    }
 }
